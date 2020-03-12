b4514fb014798d8cb2864769734f07755e21a9f5
Stanislav Kogut <stasikos@gmail.com>
2020-03-12 23:13:28 +0200

    Added timestamp to grossbook

--- Documentation/Accounting/account.sh
+++ Documentation/Accounting/account.sh
@@ -5,7 +5,8 @@
 
 LASTLINE=$(tail -1 grossbook.txt)
 CURRENTFUNDS=$(grep -A4 "name = Funding" ../../persistent.sfs | grep "funds = " | sed -re 's/.*funds = ([0-9]+)\..*/\1/')
-OLDFUNDS=$(echo "$LASTLINE" | awk '{print $1}')
+CURRENTTIME=$(grep -E "^\s+UT = " ../../persistent.sfs | sed -re 's/.*UT = ([0-9]+)\..*/\1/')
+OLDFUNDS=$(echo "$LASTLINE" | awk '{print $2}')
 
 if [ -z "$OLDFUNDS" ]; then
 	OLDFUNDS="0"
@@ -16,7 +17,7 @@ if [ $CHANGE -ne "0" ] ; then
 	echo "Now: $CURRENTFUNDS Was: $OLDFUNDS Change: $CHANGE"
 	echo "[!!!] Seems our account changed for $CHANGE, can you describe why?"
 	read REASON
-	echo -e "${CURRENTFUNDS}\t${CHANGE}\t${REASON}" >> grossbook.txt
+	echo -e "${CURRENTTIME}\t${CURRENTFUNDS}\t${CHANGE}\t${REASON}" >> grossbook.txt
 	clear
 #else
 #	echo "Seems nothing is changed, so why you bother?"
